4a249cf177d3617e5b83b6ff552e78cab368f911
rohaan_68 <muhammad6rohaan@gmail.com>
2023-10-29 16:52:19 +0500

    Initial Commit

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import olx from './1.jpg';
+// import { Link } from 'react-router-dom';
+import kfc from './1.png';
 import netflix from './2.png';
 import chatapp from './3.png';
 import ecsite from './4.png';
 import logfrm from './5.jpg';
 import blog from './6.jpg';
 const Portfolio = () => {
+  const openNewTab = () => {
+    window.open('https://kfc-clone0.vercel.app/', '_blank');
+  };
   return (
     <section className="mb-24 md:mb-[-20px]" id="portfolio">
       <div class="flex flex-col justify-center items-center">
@@ -24,17 +28,20 @@ const Portfolio = () => {
             <div class="h-full w-full">
               <img
                 class="h-full w-full transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
-                src={olx}
+                src={kfc}
                 alt=""
               />
             </div>
             <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/50 group-hover:from-black/50 group-hover:via-black/50 group-hover:to-black/50"></div>
             <div class="absolute inset-0 translate-y-[75%] sm:translate-y-[86%] md:translate-y-[79%] lg:translate-y-[82%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-[30px] sm:group-hover:translate-y-[80px]">
               <h1 class="font-poppin text-2xl text-white mb-3 sm:mb-0">
-                OLX Clone
+                KFC Clone
               </h1>
 
-              <button class="rounded-[10px] bg-white text-black font-[700] tracking-[1px] text-[15px] pb-1 pt-2 px-[14px] mt-5 mb-32 font-roboto duration-300 hover:bg-gray-800 hover:text-white shadow shadow-black/60">
+              <button
+                onClick={openNewTab}
+                class="rounded-[10px] bg-white text-black font-[700] tracking-[1px] text-[15px] pb-1 pt-2 px-[14px] mt-5 mb-32 font-roboto duration-300 hover:bg-gray-800 hover:text-white shadow shadow-black/60"
+              >
                 LIVE
               </button>
             </div>
@@ -55,7 +62,7 @@ const Portfolio = () => {
               </h1>
 
               <button class="rounded-[10px] bg-white text-black font-[700] tracking-[1px] text-[15px] pb-1 pt-2 px-[14px] mt-5 mb-32 font-roboto duration-300 hover:bg-gray-800 hover:text-white shadow shadow-black/60">
-                LIVE
+                Soon!
               </button>
             </div>
           </div>
@@ -74,7 +81,7 @@ const Portfolio = () => {
               </h1>
 
               <button class="rounded-[10px] bg-white text-black font-[700] tracking-[1px] text-[15px] pb-1 pt-2 px-[14px] mt-5 mb-32 font-roboto duration-300 hover:bg-gray-800 hover:text-white shadow shadow-black/60">
-                LIVE
+                Soon!
               </button>
             </div>
           </div>
@@ -87,13 +94,13 @@ const Portfolio = () => {
               />
             </div>
             <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/50 group-hover:from-black/50 group-hover:via-black/50 group-hover:to-black/50"></div>
-            <div class="absolute inset-0 translate-y-[75%] sm:translate-y-[88%] md:translate-y-[81%] lg:translate-y-[84%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-[30px] sm:group-hover:translate-y-[80px]">
+            <div class="absolute inset-0 translate-y-[80%] sm:translate-y-[88%] md:translate-y-[81%] lg:translate-y-[84%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-[30px] sm:group-hover:translate-y-[80px]">
               <h1 class="font-poppin text-2xl text-white mb-3 sm:mb-0">
                 Blog WebPage
               </h1>
 
               <button class="rounded-[10px] bg-white text-black font-[700] tracking-[1px] text-[15px] pb-1 pt-2 px-[14px] mt-5 mb-32 font-roboto duration-300 hover:bg-gray-800 hover:text-white shadow shadow-black/60">
-                LIVE
+                Soon!
               </button>
             </div>
           </div>
@@ -106,13 +113,13 @@ const Portfolio = () => {
               />
             </div>
             <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/50 group-hover:from-black/50 group-hover:via-black/50 group-hover:to-black/50"></div>
-            <div class="absolute inset-0 translate-y-[63%] sm:translate-y-[88%] md:translate-y-[81%] lg:translate-y-[84%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-[30px] sm:group-hover:translate-y-[80px]">
+            <div class="absolute inset-0 translate-y-[82%] sm:translate-y-[88%] md:translate-y-[81%] lg:translate-y-[84%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-[30px] sm:group-hover:translate-y-[80px]">
               <h1 class="font-poppin text-2xl text-white mb-3 sm:mb-0">
-                E-Commerce Store
+                E-Commerce
               </h1>
 
               <button class="rounded-[10px] bg-white text-black font-[700] tracking-[1px] text-[15px] pb-1 pt-2 px-[14px] mt-5 mb-32 font-roboto duration-300 hover:bg-gray-800 hover:text-white shadow shadow-black/60">
-                LIVE
+                Soon!
               </button>
             </div>
           </div>
@@ -125,13 +132,13 @@ const Portfolio = () => {
               />
             </div>
             <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/50 group-hover:from-black/50 group-hover:via-black/50 group-hover:to-black/50"></div>
-            <div class="absolute inset-0 translate-y-[75%] sm:translate-y-[88%] md:translate-y-[81%] lg:translate-y-[84%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-[30px] sm:group-hover:translate-y-[80px]">
+            <div class="absolute inset-0 translate-y-[79%] sm:translate-y-[88%] md:translate-y-[81%] lg:translate-y-[84%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-[30px] sm:group-hover:translate-y-[80px]">
               <h1 class="font-poppin text-2xl text-white mb-3 sm:mb-0">
                 Login Form
               </h1>
 
               <button class="rounded-[10px] bg-white text-black font-[700] tracking-[1px] text-[15px] pb-1 pt-2 px-[14px] mt-5 mb-32 font-roboto duration-300 hover:bg-gray-800 hover:text-white shadow shadow-black/60">
-                LIVE
+                Soon!
               </button>
             </div>
           </div>
